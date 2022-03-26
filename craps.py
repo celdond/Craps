3c1340b88f921a9d4787c7509b8b07201eecb264
celdond <84964288+celdond@users.noreply.github.com>
2022-03-26 00:29:47 -0700

    added the interactive game function to craps.py

--- craps.py
+++ craps.py
@@ -1,4 +1,4 @@
-import dice
+import dice as d
 
 class play_game:
 
@@ -30,3 +30,57 @@ class play_game:
     else:
       value = 'Try'
     return value
+    
+def interactive_game(play_game):
+
+    print('Every chip is a dollar!')
+    play_game.bankroll += int(input('How many do you want?\n'))
+    if play_game.bankroll == 0:
+      print('Alrighty. Come back to the table when you are ready.')
+
+    while True:
+      x = True
+      if play_game.bankroll == 0:
+        print('Ah, you are out of chips.')
+        print('Talk to me if you want more to keep playing.')
+        break
+
+      bet = int(input('Place your bets!\n'))
+      if bet == 0:
+        print('Thanks for playing, come back anytime.')
+        print('Remaining chips: {}\n'.format(play_game.bankroll))
+        break
+
+      player_roll = input('Alright, go ahead and roll the die.\n')
+      first_result = play_game.first_rolls()
+      # play_game.dice.display_die()
+
+      if first_result == 'Won':
+        print('Bet won!\n')
+        play_game.bankroll += bet
+        print('Current bankroll: {}'.format(play_game.bankroll))
+
+      elif first_result == 'Loss':
+        print('Ah, bet lost.\n')
+        play_game.bankroll -= bet
+        print('Current bankroll: {}'.format(play_game.bankroll))
+
+      #Section is used after first roll, but similar concept
+      else:
+        while x == True:
+          print('Trying for {}, go ahead and roll again.'.format(play_game.point))
+          input('')
+          sub_result = play_game.sub_rolls()
+          # play_game.dice.display_die()
+
+          if sub_result == 'Won':
+            print('Bet won!\n')
+            play_game.bankroll += bet
+            print('Current bankroll: {}'.format(play_game.bankroll))
+            x = False
+
+          elif sub_result == 'Loss':
+            print('Ah, bet lost.')
+            play_game.bankroll -= bet
+            print('Current bankroll: {}\n'.format(play_game.bankroll))
+            x = False
